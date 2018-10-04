e70f1a74b71116bdc4bc32aeacf264b9a27bd8d6
Minh Nhat <mocking.key118@gmail.com>
2018-10-05 00:51:08 +0700

    commit all

--- src/app/shared/config.ts
+++ src/app/shared/config.ts
@@ -1,4 +1,4 @@
 export class AppConfig {
-    public apiHost = "https://tpltesting.herokuapp.com/api";
-    public host = "https://tpltesting.herokuapp.com";
+    public apiHost = "https://security20.herokuapp.com/api";
+    public host = "https://security20.herokuapp.com";
 }
